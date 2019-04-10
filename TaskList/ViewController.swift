caafbddb98c7905256ccf684651f10b62f9d09e3
hugoiuri <hugoraiuri@gmail.com>
2019-04-09 17:25:02 -0700

    Imeplementação da adição  e remoção de itens

--- TaskList/ViewController.swift
+++ TaskList/ViewController.swift
@@ -48,9 +48,51 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         tableView.dataSource = self
         tableView.delegate = self
     }
+        
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let removeAction = UIContextualAction(
+            style: .destructive,
+            title: "Remover",
+            handler: { (action, view, completionHandler) in
+                self.items.remove(at: indexPath.row)
+                tableView.reloadData()
+                completionHandler(true)
+        })
+        
+        let swipeConfiguration = UISwipeActionsConfiguration(actions: [removeAction])
+            return swipeConfiguration
+    }
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        items[indexPath.row].isCompleted.toggle()
-        tableView.reloadData()
+    func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let concludeAction = UIContextualAction(
+            style: .normal,
+            title: "Concluir",
+            handler: { (action, view, completionHandler) in
+                self.items[indexPath.row].isCompleted.toggle()
+                tableView.reloadData()
+                completionHandler(true)
+        })
+        
+        let swipeConfiguration = UISwipeActionsConfiguration(actions: [concludeAction])
+        return swipeConfiguration
+    }
+    
+    @IBAction func addNewTodo(_ sender: Any) {
+        let alertController = UIAlertController(title: "Nova tarefa", message: "Digite a nova tarefa", preferredStyle: .alert)
+        
+        alertController.addTextField { (textField) in textField.placeholder = "Tarefa" }
+        
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: { _ in
+            guard let task = alertController.textFields?.first?.text else { return }
+            self.items.append(Todo(task: task))
+            self.tableView.reloadData()
+        })
+        
+        let cancelAction = UIAlertAction(title: "Cancelar", style: .cancel, handler: nil)
+        
+        alertController.addAction(okAction)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true, completion: nil)
     }
 }
